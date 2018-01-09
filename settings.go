adb4415d8f9be517fa456b7fb48c752056554c3f
helluvasatan <m@brel.me>
2018-01-09 13:02:33 +0500

    hack

--- settings.go
+++ settings.go
@@ -1,34 +1,14 @@
 package gopushover
 
 import (
-	"encoding/json"
-	"io/ioutil"
-	"os"
-	"os/user"
-	"path"
-	"path/filepath"
+	"github.com/minya/goutils/config"
 	"strings"
 )
 
 func ReadSettings(spath string) (PushoverSettings, error) {
 	var settings PushoverSettings
 
-	if strings.Index(spath, "~/") == 0 {
-		user, _ := user.Current()
-		spath = path.Join(user.HomeDir, strings.TrimLeft(spath, "~/"))
-	} else if strings.Index(spath, "/") != 0 {
-		exe, err := os.Executable()
-		if err == nil {
-			spath = path.Join(filepath.Dir(exe), spath)
-		}
-	}
-
-	settingsBin, errRead := ioutil.ReadFile(spath)
-	if nil != errRead {
-		return settings, errRead
-	}
-
-	errUnmarshal := json.Unmarshal(settingsBin, &settings)
+	errUnmarshal := config.UnmarshalJson(&settings, strings.TrimLeft(spath, "~/"))
 	if nil != errUnmarshal {
 		return settings, errUnmarshal
 	}
